b67a226fedf36faceebbe9c7977484b4dbe36e89
gvijay-tech <60471164+gvijay-tech@users.noreply.github.com>
2020-05-17 21:06:34 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -48,7 +48,7 @@ async def rename_doc(bot, update):
         if len(file_name) > 64:
             await update.reply_text(
                 Translation.IFLONG_FILE_NAME.format(
-                    alimit="64",
+                    alimit="150",
                     num=len(file_name)
                 )
             )
